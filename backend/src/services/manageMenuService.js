2b5e6f96c82045e1036dd5fd6b59a79922db707b
Chai Pin Zheng <pinzheng61.20@ichat.sp.edu.sg>
2021-09-23 17:14:33 +0800

    [BUG] Resolving Derrick's unhandled promise rejection

--- backend/src/services/manageMenuService.js
+++ backend/src/services/manageMenuService.js
@@ -2,34 +2,28 @@ const config = require('../config/config');
 const pool = require('../config/database');
 
 module.exports.getMenuCategory = (email, callback) => {
-    return new Promise((resolve, reject) => {
-        pool.getConnection((err, connection) => {
-            if (err) {
-                console.log("Database connection error ", err);
-                resolve(err);
-            } else {
-                try {
+    try {
+        return new Promise((resolve, reject) => {
+            pool.getConnection((err, connection) => {
+                if (err) {
+                    console.log("Database connection error ", err);
+                    resolve(err);
+                } else {
                     let query = `SELECT catname FROM category`;
                     connection.query(query, [], (err, result) => {
                         if (err) {
-                            console.log(err);
                             reject(err);
                         } else {
-                            if (result.length == 1) {
-                                resolve(result);
-                            } else {
-                                reject(result);
-                            }
+                            resolve(result)
                         }
                         connection.release();
                     });
-                } catch (error) {
-                    console.log(err);
-                    resolve(err);
                 }
-            }
+            });
         });
-    }); 
+    } catch (error) {
+        reject(error)
+    }
 }
 
 module.exports.getMenuProducts = (email, callback) => {
@@ -41,7 +35,7 @@ module.exports.getMenuProducts = (email, callback) => {
             } else {
                 try {
                     let query =
-                                `SELECT product_title, brief_description, retail_price, stock_quantity FROM products`;
+                        `SELECT product_title, brief_description, retail_price, stock_quantity FROM products`;
                     connection.query(query, [], (err, result) => {
                         if (err) {
                             console.log(err);
@@ -61,5 +55,5 @@ module.exports.getMenuProducts = (email, callback) => {
                 }
             }
         });
-    }); 
+    });
 }
